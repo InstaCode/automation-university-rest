7b97923d903be239fdada3ce8b8a21530493b8fa
Jason Edstrom <jason@instacode.io>
2020-12-29 17:14:20 -0600

    Removed setup classes

--- database/src/main/java/io/instacode/university/database/user/repositories/UserEmailRepository.java
+++
@@ -1,7 +0,0 @@
-package io.instacode.university.database.user.repositories;
-
-import io.instacode.university.database.user.models.UserEmail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserEmailRepository extends JpaRepository<UserEmail, Long> {
-}
--- database/src/main/java/io/instacode/university/database/user/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.instacode.university.database.user.repositories;
-
-import io.instacode.university.database.user.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
